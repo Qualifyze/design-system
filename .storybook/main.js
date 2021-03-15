13e6f8f58a650aaf680088f78e3d0e161fc7f402
Gregor Albrecht <hello@gregoralbrecht.com>
2021-03-15 12:14:08 +0100

    Create skeleton for Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,15 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: [
+    `../src/**/*.stories.@(ts|js|md)x`,
+    `./components/**/*.stories.@(ts|js|md)x`,
+  ],
+  addons: [
+    `@storybook/addon-links`,
+    `@storybook/addon-essentials`,
+    `@storybook/addon-knobs`,
+  ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,53 @@
+import '@fontsource/source-sans-pro';
+
+import './components/global-theme-styles.css';
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
+  layout: `centered`,
+  options: {
+    storySort: {
+      method: `alphabetical`,
+      order: [`Design Tokens`, `Foundations`, `Components`],
+    },
+  },
+  viewport: {
+    // defaultViewport: `responsive`,
+    viewports: {
+      initial: {
+        name: `Initial (320px)`,
+        styles: { width: `320px`, height: `100%` },
+        type: 'desktop',
+      },
+      small: {
+        name: `Small (640px)`,
+        styles: { width: `640px`, height: `100%` },
+        type: 'desktop',
+      },
+      medium: {
+        name: `Medium (768px)`,
+        styles: { width: `1024px`, height: `100%` },
+        type: 'desktop',
+      },
+      large: {
+        name: `Large (1024px)`,
+        styles: { width: `1024px`, height: `100%` },
+        type: 'desktop',
+      },
+      extraLarge: {
+        name: `XLarge (1280px)`,
+        styles: { width: `1280px`, height: `100%` },
+        type: 'desktop',
+      },
+      extraExtraLarge: {
+        name: `XXLarge (1536px)`,
+        styles: { width: `1536px`, height: `100%` },
+        type: 'desktop',
+      },
+    },
+  },
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
-  actions: { argTypesRegex: '^on.*' },
+  actions: { argTypesRegex: `^on.*` },
 };
+
+export const decorators = [];
