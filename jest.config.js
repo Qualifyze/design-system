8ae574da9912b3f62d42ebb5f7f557a8b81ef111
Philipp Jardas <philipp@jardas.de>
2021-06-25 13:06:00 +0200

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,10 @@
 module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
   testURL: `http://localhost`,
   setupFilesAfterEnv: [`<rootDir>/src/util/test/setupTests.js`],
-  /**
-   * Enable absolute imports from specific entry points.
-   * @example: `import Button from '@ds/components/Button'`
-   * @see: https://bit.ly/2KgcOOY
-   */
   moduleNameMapper: {
-    // Stubs
-    '\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
+    '\\.css$': 'identity-obj-proxy',
+    '^typeface-source-sans-pro$': 'identity-obj-proxy',
   },
 }
