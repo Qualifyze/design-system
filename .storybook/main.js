e27146f4ac1ed02cd6dc4965c5d9a3c859439c4f
Gregor Albrecht <hello@gregoralbrecht.com>
2021-07-14 18:31:38 +0200

    Move React components into `/packages`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,7 @@
 module.exports = {
   stories: [
-    `../src/**/*.stories.@(ts|js|md)x`,
-    `./components/**/*.stories.@(ts|js|md)x`,
+    './components/**/*.stories.mdx',
+    '../packages/react-components/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
-  addons: [
-    `@storybook/addon-links`,
-    `@storybook/addon-essentials`,
-    `@storybook/addon-knobs`,
-  ],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-  typescript: {
-    check: true, // type-check stories during Storybook build
-  },
-};
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,11 @@
-import '@fontsource/source-sans-pro';
+import '@fontsource/source-sans-pro'
 
-import { ThemeProvider, theme } from '../src/theme/theme';
-import '../src/styles/normalize.css';
-import './components/global-theme-styles.css';
+import {
+  ThemeProvider,
+  theme,
+} from '../packages/react-components/src/theme/theme'
+import '../packages/react-components/src/styles/normalize.css'
+import './components/global-theme-styles.css'
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
@@ -50,12 +53,12 @@ export const parameters = {
   },
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: `^on.*` },
-};
+}
 
-const withTheme = Story => (
+const withTheme = (Story) => (
   <ThemeProvider theme={theme}>
     <Story />
   </ThemeProvider>
-);
+)
 
-export const decorators = [withTheme];
+export const decorators = [withTheme]
