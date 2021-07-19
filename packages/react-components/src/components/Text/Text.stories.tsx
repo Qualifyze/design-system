8d8483d17d8e8129a5ff771817ef4dfea6a4798c
Gregor Albrecht <hello@gregoralbrecht.com>
2021-07-19 16:27:55 +0200

    Add Text and capsize

---
+++ packages/react-components/src/components/Text/Text.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Text } from './Text';
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+};
+
+export const basic = () => (
+  <>
+    <Text size="tiny">Hola mundo</Text>
+    <Text size="tiny">Hola mundo</Text>
+    <Text size="tiny">Hola mundo</Text>
+    <Text size="small">Hola mundo</Text>
+    <Text size="small">Hola mundo</Text>
+    <Text size="small">Hola mundo</Text>
+    <Text size="standard">Hola mundo</Text>
+    <Text size="standard">Hola mundo</Text>
+    <Text size="standard">Hola mundo</Text>
+    <Text size="large">Hola mundo</Text>
+    <Text size="large">Hola mundo</Text>
+    <Text size="large">Hola mundo</Text>
+  </>
+);
+
+export const paragraphs = () => (
+  <>
+    <Text>
+      Pivot termsheet twitter. Leverage deployment holy grail advisor traction
+      monetization seed money business model canvas validation ownership pitch
+      infographic. Conversion direct mailing social proof release validation
+      disruptive termsheet alpha leverage long tail hackathon iteration user
+      experience. Hypotheses advisor paradigm shift hackathon interaction design
+      ecosystem sales research & development business plan iteration.
+    </Text>
+    <Text>
+      Equity startup alpha hypotheses accelerator iPad. Gamification holy grail
+      MVP business model canvas funding pitch responsive web design mass market
+      paradigm shift virality. Termsheet customer user experience return on
+      investment metrics interaction design validation hackathon social media
+      direct mailing client funding value proposition business plan. Disruptive
+      market low hanging fruit prototype paradigm shift.
+    </Text>
+    <Text>
+      Branding focus buzz niche market handshake android virality user
+      experience. MVP crowdfunding incubator first mover advantage strategy
+      disruptive hypotheses founders long tail focus venture.
+    </Text>
+  </>
+);
---
+++ packages/react-components/src/components/Text/Text.tsx
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import type * as Polymorphic from '@radix-ui/react-polymorphic';
+// import responsiveCapsize from 'responsive-capsize';
+import capsize from 'capsize';
+
+import { styled, CSS, StitchesVariants } from '../../stitches.config';
+
+const DEFAULT_TAG = 'p';
+
+// Font metrics for "Inter"
+const fontMetrics = {
+  capHeight: 2048,
+  ascent: 2728,
+  descent: -680,
+  lineGap: 0,
+  unitsPerEm: 2816,
+};
+
+const tinyStyles = capsize({
+  capHeight: 8,
+  lineGap: 10,
+  fontMetrics,
+});
+
+const smallStyles = capsize({
+  capHeight: 10,
+  lineGap: 12,
+  fontMetrics,
+});
+
+const standardStyles = capsize({
+  capHeight: 12,
+  lineGap: 18,
+  fontMetrics,
+});
+
+const largeStyles = capsize({
+  capHeight: 14,
+  lineGap: 20,
+  fontMetrics,
+});
+
+console.log(tinyStyles);
+
+export const StyledText = styled(DEFAULT_TAG, {
+  // Reset
+  lineHeight: '1',
+  margin: '0',
+  fontWeight: 400,
+  fontVariantNumeric: 'tabular-nums',
+  display: 'block',
+  marginBlockStart: '0',
+  marginBlockEnd: '0',
+  marginInlineStart: '0',
+  marginInlineEnd: '0',
+
+  variants: {
+    size: {
+      tiny: {
+        ...tinyStyles,
+        '&::before': {
+          ...tinyStyles['::before'],
+        },
+        '&::after': {
+          ...tinyStyles['::after'],
+        },
+      },
+      small: {
+        ...smallStyles,
+        '&::before': {
+          ...smallStyles['::before'],
+        },
+        '&::after': {
+          ...smallStyles['::after'],
+        },
+      },
+      standard: {
+        ...standardStyles,
+        '&::before': {
+          ...standardStyles['::before'],
+        },
+        '&::after': {
+          ...standardStyles['::after'],
+        },
+      },
+      large: {
+        ...largeStyles,
+        '&::before': {
+          ...largeStyles['::before'],
+        },
+        '&::after': {
+          ...largeStyles['::after'],
+        },
+      },
+    },
+  },
+  compoundVariants: [],
+  defaultVariants: {
+    size: 'standard',
+  },
+});
+
+type TextCSSProp = { css?: CSS };
+// TODO: Remove omit fix when this is merged https://github.com/modulz/stitches/issues/421
+type TextVariants = Omit<StitchesVariants<typeof StyledText>, 'size'>;
+type TextOwnProps = TextCSSProp & TextVariants & { size?: any };
+
+type TextComponent = Polymorphic.ForwardRefComponent<
+  typeof DEFAULT_TAG,
+  TextOwnProps
+>;
+
+export const Text = React.forwardRef((props, forwardedRef) => {
+  return <StyledText {...props} ref={forwardedRef} />;
+}) as TextComponent;
+
+Text.toString = () => `.${StyledText.className}`;
