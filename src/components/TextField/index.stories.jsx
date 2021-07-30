732e04e647e60883e7d7a7567b22bf163ac393d5
Vladimir Ubogovich <vladimir.ubogovich@qualifyze.com>
2021-07-30 12:01:49 +0200

    Fix branch name in GitHub workflow

--- src/components/TextField/index.stories.jsx
+++ src/components/TextField/index.stories.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import { text, number } from '@storybook/addon-knobs'
+import { text } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
