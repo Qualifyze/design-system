6b8e75ef2f70501cf99c8a481ed1b122878c6a6d
Gregor Albrecht <hello@gregoralbrecht.com>
2021-08-04 15:41:21 +0200

    Build and publish `design-tokens`

---
+++ packages/design-tokens/src/tokens/colors.ts
@@ -0,0 +1,29 @@
+const brand = {
+  brand1: 'hsl(221, 100%, 97%)',
+  brand2: 'hsl(223, 59%, 86%)',
+  brand3: 'hsl(223, 44%, 71%)',
+  brand4: 'hsl(223, 40%, 60%)',
+  brand5: 'hsl(223, 36%, 50%)',
+  brand6: 'hsl(223, 47%, 39%)',
+  brand7: 'hsl(223, 60%, 29%)',
+  brand8: 'hsl(223, 70%, 21%)',
+  brand9: 'hsl(223, 79%, 13%)',
+}
+
+// Taken from https://github.com/radix-ui/colors/blob/5d794765bf54b5af431e17a8bcca863a695a4cf4/src/light/gray.ts
+const gray = {
+  gray1: 'hsl(0 0% 99.0%)',
+  gray2: 'hsl(0 0% 97.3%)',
+  gray3: 'hsl(0 0% 95.1%)',
+  gray4: 'hsl(0 0% 93.0%)',
+  gray5: 'hsl(0 0% 90.9%)',
+  gray6: 'hsl(0 0% 88.7%)',
+  gray7: 'hsl(0 0% 85.8%)',
+  gray8: 'hsl(0 0% 78.0%)',
+  gray9: 'hsl(0 0% 56.1%)',
+  gray10: 'hsl(0 0% 52.3%)',
+  gray11: 'hsl(0 0% 43.5%)',
+  gray12: 'hsl(0 0% 9.0%)',
+}
+
+export const colors = { brand, gray }
