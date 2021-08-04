2a557209cdadfd1ee56199f6eef64acbf5791bc7
Gareth Fuller <gareth.fuller@qualifyze.com>
2021-08-04 15:15:51 +0200

    QC-1181: Add select field creatable component (#110)
    * Add select field creatable component
    
    * fix little bug with adding a new row each time to the select
    
    * moar code review changes
    
    * add the ability to pass in a custom label for the "create new" text
    
    * some feedback from pablo
    
    * add the same on blur fix gregor introduced in another PR
    
    * add spacer char for the input
    
    * A method that at least works but involves some state :/
    
    * Remove React state
    
    Co-authored-by: Gregor Albrecht <hello@gregoralbrecht.com>

---
+++ src/components/SelectCreatableField/index.jsx
@@ -0,0 +1,218 @@
+import React from 'react'
+import CreatableSelect from 'react-select/creatable'
+import { useField } from 'formik'
+import PropTypes from 'prop-types'
+
+import FieldMessage from '../FieldMessage'
+import FieldLabel from '../FieldLabel'
+import Text from '../Text'
+import Flex from '../Flex'
+
+import baseTheme from './baseTheme'
+import Wrapper from './Wrapper'
+
+const spacing = {
+  baseUnit: 4,
+  controlHeight: 38,
+  menuGutter: 8,
+}
+/**
+ * Theming colors guide for react-select
+ * primary:  control and option components selected
+ * primary25: hover over option
+ * neutral30: hover over control component
+ * neutral20: control component border
+ * neutral80: font color of value container
+ * neutral0: background control
+ */
+const defaultVariant = customTheme => ({
+  borderRadius: '5px',
+  spacing,
+  colors: {
+    primary: customTheme.selected,
+    primary25: customTheme.hoverOverOption,
+    neutral80: customTheme.valueContainerTextColor,
+    neutral30: customTheme.hoverOverControl,
+    neutral20: customTheme.controlBorder,
+    neutral0: customTheme.backGroundControl,
+  },
+  boxShadowMenuIsOpen: 'inset 0 0 0 1px',
+  boxShadowIsFocused: 'inset 0 0 0 1px',
+  boxShadowDefault: 'inset 0 0 0 1px',
+  boxShadowHover: 'inset 0 0 0 1px',
+})
+const errorVariant = customTheme => ({
+  borderRadius: '5px',
+  spacing,
+  colors: {
+    primary: customTheme.selectedError,
+    primary25: customTheme.hoverOverOption,
+    neutral80: customTheme.valueContainerTextColor,
+    neutral30: customTheme.hoverOverControlError,
+    neutral20: customTheme.controlBorderError,
+    neutral0: customTheme.backGroundControlError,
+  },
+  boxShadowMenuIsOpen: 'inset 0 0 0 2px',
+  boxShadowIsFocused: 'inset 0 0 0 2px',
+  boxShadowDefault: 'inset 0 0 0 2px',
+  boxShadowHover: 'inset 0 0 0 2px',
+})
+const customStyles = {
+  control: (provided, state) => {
+    const styles = {
+      ...provided,
+      'width': '100%',
+      'color': state.isDisabled && `${baseTheme.placeholderColor}`,
+      'background':
+        (state.isDisabled && `${baseTheme.disabledBackground}`) ||
+        state.theme.colors.neutral0,
+      'minHeight': 50,
+      'borderWidth': '0px',
+      'boxShadow':
+        (state.menuIsOpen &&
+          `${baseTheme.shadowFocusRing}, ${state.theme.boxShadowMenuIsOpen} ${state.theme.colors.primary}`) ||
+        (state.isFocused &&
+          `${state.theme.boxShadowIsFocused} ${state.theme.colors.neutral30}`) ||
+        `${state.theme.boxShadowDefault} ${state.theme.colors.neutral20}`,
+      '&:hover': {
+        boxShadow:
+          !state.menuIsOpen &&
+          `${state.theme.boxShadowHover} ${state.theme.colors.neutral30}`,
+      },
+      '&:focus-within': {
+        boxShadow: `${baseTheme.shadowFocusRing}, inset 0 0 0 1px ${state.theme.colors.neutral30}`,
+      },
+    }
+    if (state.isDisabled) styles.borderColor = `${baseTheme.disabled}`
+    return styles
+  },
+  valueContainer: provided => ({ ...provided }),
+  input: () => ({
+    margin: 0,
+  }),
+  menu: provided => ({
+    ...provided,
+    zIndex: 3,
+    // The following values are taken from theme, but copy&pasted for ease
+    marginTop: 4,
+    borderRadius: 4,
+    boxShadow: `0 0 0 1px hsl(210, 24%, 87%), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)`,
+  }),
+  menuList: provided => ({
+    ...provided,
+    maxHeight: 200,
+  }),
+  placeholder: provided => ({
+    ...provided,
+    color: baseTheme.placeholderColor,
+    margin: 0,
+    marginLeft: '-1px',
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    cursor: 'pointer',
+    color: state.isSelected
+      ? baseTheme.optionTextSelected
+      : baseTheme.optionTextColor,
+  }),
+  dropdownIndicator: (provided, state) => ({
+    ...provided,
+    color:
+      (state.isFocused && state.theme.colors.primary) ||
+      baseTheme.placeholderColor,
+  }),
+  indicatorSeparator: () => ({ display: 'none' }),
+}
+const SelectCreatableField = ({
+  options,
+  name,
+  label,
+  placeholder,
+  disabled,
+  size,
+  menuPlacement,
+  createNewLabelText,
+  onCreateOption,
+}) => {
+  const [field, meta, helpers] = useField({ name })
+
+  return (
+    <Wrapper size={size}>
+      <FieldLabel htmlFor={name} label={label} />
+      <CreatableSelect
+        options={options}
+        placeholder={placeholder}
+        name={field.name}
+        formatCreateLabel={input => {
+          return (
+            <Flex>
+              {`${createNewLabelText}:`}
+              <Text weight="medium">&nbsp;{`"${input}"`}</Text>
+            </Flex>
+          )
+        }}
+        onBlur={() => {
+          helpers.setTouched(true)
+        }}
+        onChange={option => helpers.setValue(option.value)}
+        onCreateOption={newValue => {
+          helpers.setValue(newValue)
+          onCreateOption?.()
+        }}
+        // We need to sync the Formik state with the `react-select` input. To do
+        // that, we need to find out whether the user...
+        //    a) ...chose an option of the passed `options` list or
+        //    b) ...created their own option or
+        //    c) ...the input is currently empty/undefined
+        value={(() => {
+          const foundOption = options.find(o => o.value === field.value)
+          switch (true) {
+            // a)
+            case !!foundOption:
+              return foundOption
+            // b)
+            case !!field.value:
+              return { label: field.value, value: field }
+            // c)
+            default:
+              return ''
+          }
+        })()}
+        styles={customStyles}
+        theme={
+          (meta.error && meta.touched && errorVariant(baseTheme)) ||
+          defaultVariant(baseTheme)
+        }
+        isDisabled={disabled}
+        menuPlacement={menuPlacement}
+        createOptionPosition="first"
+      />
+      {meta.error && meta.touched && (
+        <FieldMessage tone="critical" message={meta.error} />
+      )}
+    </Wrapper>
+  )
+}
+SelectCreatableField.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({ label: PropTypes.string, value: PropTypes.string })
+  ).isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  size: PropTypes.oneOf(['tiny', 'small', 'standard', 'large']),
+  menuPlacement: PropTypes.oneOf(['auto', 'top', 'bottom']),
+  createNewLabelText: PropTypes.string,
+  onCreateOption: PropTypes.func,
+}
+SelectCreatableField.defaultProps = {
+  placeholder: '',
+  disabled: false,
+  size: 'standard',
+  label: '',
+  menuPlacement: 'auto',
+  createNewLabelText: 'Create',
+}
+
+export default SelectCreatableField
---
+++ src/components/SelectCreatableField/index.stories.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { boolean, text, select } from '@storybook/addon-knobs'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import { action } from '@storybook/addon-actions'
+
+import Button from '../Button'
+import FormDebugger from '../FormDebugger'
+import Box from '../Box'
+import Stack from '../Stack'
+import Actions from '../Actions'
+
+import SelectCreatableField from './index'
+
+export default {
+  title: 'SelectCreatableField',
+  component: SelectCreatableField,
+}
+
+const options = [
+  {
+    label: 'Avengers 1',
+    value: 'avengers',
+  },
+  {
+    label: 'Groundhog Day',
+    value: 'groundHogDay',
+  },
+  {
+    label: 'Up',
+    value: 'Up',
+  },
+  {
+    label: 'The Last Dance',
+    value: 'theLastDance',
+  },
+  {
+    label: 'El Secreto de sus Ojos',
+    value: 'elSecretoDeSusOjos',
+  },
+]
+
+const selectSchema = Yup.object().shape({
+  movie: Yup.string().required(),
+})
+
+export const Default = () => {
+  const insideState = boolean('Inside State', false)
+  const disabled = boolean('Disabled', false)
+  const placeholder = text('Placeholder', 'Select or create a movie')
+  const createNewLabelText = text('Create new label', undefined)
+  const availableSizes = ['tiny', 'small', 'standard', 'large']
+  const size = select('Size', availableSizes, 'standard')
+
+  return (
+    <Formik
+      initialValues={{
+        movie: '',
+      }}
+      onSubmit={values => {
+        action(`Submitted! ${JSON.stringify(values, undefined, 2)}`)
+      }}
+      validationSchema={selectSchema}
+    >
+      {({ resetForm }) => (
+        <Form>
+          <Box sx={{ p: 4 }}>
+            <Stack space={3}>
+              <Box>
+                <SelectCreatableField
+                  options={options}
+                  name="movie"
+                  label="What is your favorite movie?"
+                  placeholder={placeholder}
+                  disabled={disabled}
+                  size={size}
+                  createNewLabelText={createNewLabelText}
+                />
+              </Box>
+              <Box>
+                <Actions>
+                  <Button type="submit">Submit</Button>
+                  <Button
+                    variant="secondary"
+                    onClick={event => {
+                      event.preventDefault()
+                      resetForm()
+                    }}
+                  >
+                    Reset
+                  </Button>
+                </Actions>
+              </Box>
+              {insideState && <FormDebugger />}
+            </Stack>
+          </Box>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+Default.story = {
+  name: 'default',
+}
