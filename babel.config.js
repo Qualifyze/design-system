ee3ec054eb2c35f262e64e4b8228479c54476099
Vladimir Ubogovich <vladimir.ubogovich@qualifyze.com>
2021-08-17 19:02:28 +0200

    Bundle with zero-config microbundle

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,12 @@
-module.exports = api => ({
+module.exports = {
   presets: [
     [
       '@babel/preset-env',
       {
-        modules: api.env('test') ? 'cjs' : false,
         useBuiltIns: 'usage',
         corejs: 3,
       },
     ],
     '@babel/preset-react',
   ],
-  ignore: api.env('test') ? [] : ['**/*.test.jsx', 'src/util/test'],
-})
+}
