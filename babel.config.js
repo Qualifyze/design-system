9dc81f475e0102684b5fedd3237ff2d2de380227
Gregor Albrecht <hello@gregoralbrecht.com>
2021-08-18 11:32:27 +0200

    Slim down babel config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
-module.exports = api => ({
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'usage',
-        corejs: 3,
-      },
-    ],
-    '@babel/preset-react',
-  ],
-  ignore: api.env('test') ? [] : ['**/*.test.jsx', 'src/util/test'],
+module.exports = () => ({
+  presets: ['@babel/preset-react'],
 })
