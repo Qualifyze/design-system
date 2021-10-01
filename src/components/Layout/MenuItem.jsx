b34368bc6859fe00b6ef6d2d6b94ca10cfcb9c66
Philipp Jardas <philipp.jardas@qualifyze.com>
2021-10-01 12:30:18 +0200

    Layout: add toggle (QC-1275)

--- src/components/Layout/MenuItem.jsx
+++ src/components/Layout/MenuItem.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 
+import Box from '../Box'
 import Text from '../Text'
 
-export function MenuItem({ children, active }) {
+import { useDrawerContext } from './Context'
+
+export function MenuItem({ children, active, as: Comp, icon, ...props }) {
+  const { collapsed } = useDrawerContext()
+
+  // Hide items without an icon in collapsed state
+  if (collapsed && !icon) return null
+
   return (
     <Text
       as="li"
@@ -11,30 +19,46 @@ export function MenuItem({ children, active }) {
       sx={{
         color: 'inherit',
         a: {
-          'display': 'flex',
-          'alignItems': 'baseline',
-          'color': 'inherit',
-          'textDecoration': 'none',
-          'px': 3,
-          'py': 2,
-          'bg': active ? 'secondary.600' : 'transparent',
-          '&:hover': {
-            bg: 'secondary.700',
-          },
-          'transition': 'background-color 100ms linear',
+          color: 'inherit',
         },
       }}
     >
-      {children}
+      <Comp {...props}>
+        <Box
+          sx={{
+            'display': 'flex',
+            'alignItems': 'baseline',
+            'justifyContent': collapsed ? 'center' : 'flex-start',
+            'color': 'inherit',
+            'textDecoration': 'none',
+            'px': collapsed ? 2 : 3,
+            'py': 2,
+            'bg': active ? 'secondary.600' : 'transparent',
+            '&:hover': {
+              bg: 'secondary.700',
+            },
+            'transition': 'background-color 100ms linear',
+          }}
+        >
+          {icon ? (
+            <Box sx={{ mr: collapsed ? undefined : 2 }}>{icon}</Box>
+          ) : null}
+          {collapsed ? null : children}
+        </Box>
+      </Comp>
     </Text>
   )
 }
 
 MenuItem.propTypes = {
-  children: PropTypes.node.isRequired,
   active: PropTypes.bool,
+  as: PropTypes.elementType,
+  children: PropTypes.node.isRequired,
+  icon: PropTypes.node,
 }
 
 MenuItem.defaultProps = {
   active: false,
+  as: 'a',
+  icon: undefined,
 }
