22c35e4f99bca114b8ff6ea5d48138a25f4f247a
Gregor Albrecht <hello@gregoralbrecht.com>
2021-10-18 16:25:10 +0200

    Add example for custom item in `DropdownMenu`

--- src/components/DropdownMenu/index.stories.jsx
+++ src/components/DropdownMenu/index.stories.jsx
@@ -1,12 +1,16 @@
+/* eslint-disable no-alert */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 // import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 import { styled } from '../../util/style'
 import Box from '../Box'
+import Flex from '../Flex'
 import Button from '../Button'
 import Icon from '../Icon'
 import Inline from '../Inline'
+import Stack from '../Stack'
+import Text from '../Text'
 
 import { DropdownMenu } from './index'
 
@@ -74,4 +78,65 @@ export const Default = () => {
 }
 Default.story = {
   name: 'default',
+}
+
+export const CustomItem = () => {
+  return (
+    <Box sx={{ minHeight: '500px' }}>
+      <DropdownMenu>
+        <Inline>
+          <DropdownMenu.Trigger asChild>
+            <Button
+              variant="secondary"
+              size="small"
+              icon={<Icon name="chevrondown" size="tiny" />}
+            >
+              Manage account
+            </Button>
+          </DropdownMenu.Trigger>
+        </Inline>
+
+        <DropdownMenu.Content align="start">
+          <DropdownMenu.Item disabled>
+            <Flex sx={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Box sx={{ py: 2, pr: 2 }}>
+                <Flex
+                  sx={{
+                    height: '30px',
+                    width: '30px',
+                    borderRadius: '50%',
+                    bg: 'primary.900',
+                    color: 'white',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  WW
+                </Flex>
+              </Box>
+              <Box>
+                <Stack space={1}>
+                  <Text size="small" weight="medium">
+                    Willy Wonka
+                  </Text>
+                  <Text size="small">willy.wonka@qualifyze.com</Text>
+                </Stack>
+              </Box>
+            </Flex>
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item>Manage profile</DropdownMenu.Item>
+          <DropdownMenu.Item onClick={() => alert('logged out')}>
+            Log out{' '}
+            <RightSlot>
+              <Icon name="exit" />
+            </RightSlot>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu>
+    </Box>
+  )
+}
+CustomItem.story = {
+  name: 'with custom item',
 }
