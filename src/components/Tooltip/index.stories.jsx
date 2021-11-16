89e7167677c65adbff8b5964a0cdeefb99e7aa9b
Gregor Albrecht <hello@gregoralbrecht.com>
2021-11-16 12:15:56 +0100

    Use Radix UI for Tooltip
    Our tooltip was previously using Reach UI, which still works perfectly
    fine. However, the API of the Radix one is just ✨ so let's use it.

--- src/components/Tooltip/index.stories.jsx
+++ src/components/Tooltip/index.stories.jsx
@@ -1,28 +1,59 @@
 import React from 'react'
 import { text, select } from '@storybook/addon-knobs'
 
-import Box from '../Box'
+import Flex from '../Flex'
+import Inline from '../Inline'
+import Button from '../Button'
 
-import Tooltip from './index'
+import { Tooltip } from './index'
 
 export default { title: 'Tooltip', component: Tooltip }
 
+const buttonPlacementToFlex = {
+  'center': { justifyContent: 'center', alignItems: 'center' },
+  'left': { justifyContent: 'flex-start', alignItems: 'center' },
+  'top left': { justifyContent: 'flex-start', alignItems: 'flex-start' },
+  'bottom': { justifyContent: 'center', alignItems: 'flex-end' },
+}
+
 export const Default = () => {
-  const textExample = text('Text', 'Some text as Example')
-  const tooltipText = text('Tooltip Text', 'Some text to test')
-  const variant = select('Variant', ['default', 'light'], 'default')
-  const indicator = text('Indicator', 'chevronUp')
+  const tooltipText = text('Tooltip Text', 'Do NOT click this button...')
+  const buttonPlacement = select(
+    'Button placement',
+    ['center', 'left', 'top left', 'bottom'],
+    'center'
+  )
+  const side = select('Side', ['top', 'left', 'right', 'bottom'], 'bottom')
 
   return (
-    <Box mx={7} my={5}>
-      <Tooltip
-        dataTooltip={tooltipText}
-        indicator={indicator}
-        variant={variant}
-      >
-        {textExample}
-      </Tooltip>
-    </Box>
+    <Flex
+      sx={{
+        boxSizing: 'border-box',
+        position: 'absolute',
+        inset: 0,
+        display: 'flex',
+        ...buttonPlacementToFlex[buttonPlacement],
+      }}
+    >
+      <Inline>
+        <Tooltip>
+          <Tooltip.Trigger asChild>
+            <Button
+              onClick={() => {
+                // eslint-disable-next-line no-alert
+                alert('Why? Just... why did you do this?')
+              }}
+            >
+              Send missile alert (test)
+            </Button>
+          </Tooltip.Trigger>
+          <Tooltip.Content sideOffset={5} side={side}>
+            {tooltipText}
+            <Tooltip.Arrow offset={10} />
+          </Tooltip.Content>
+        </Tooltip>
+      </Inline>
+    </Flex>
   )
 }
 Default.story = {
