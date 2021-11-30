c6e89c699a1c3b126994bcbca7e6f879c8a121e2
Gregor Albrecht <hello@gregoralbrecht.com>
2021-11-30 20:13:38 +0100

    Fix styles

--- src/components/Tabs/index.stories.jsx
+++ src/components/Tabs/index.stories.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Box from '../Box'
+import Container from '../Container'
 import Heading from '../Heading'
 import Text from '../Text'
 import TextLink from '../TextLink'
 import List from '../List'
+import Stack from '../Stack'
 
 import { Tabs, TabList, TabContent, TabTrigger } from '.'
 
@@ -56,31 +58,68 @@ Default.story = {
   name: 'default',
 }
 
-export const WithOverview = () => {
+const cardStyles = { borderRadius: 3, px: 4, py: 3, bg: 'white', mb: 3 }
+
+export const Example = () => {
   return (
-    <Tabs defaultValue="requests">
-      <Box sx={{ bg: 'secondary.100' }}>
-        <Box sx={{ py: 4, px: 3 }}>
-          <Heading level={3}>
-            A title that is align with the first TabTrigger
-          </Heading>
+    <Box sx={{ minHeight: '100%', bg: 'background.100' }}>
+      <Tabs defaultValue="requests">
+        <Box sx={{ bg: 'white' }}>
+          <Container maxWidth="page">
+            <Box sx={{ py: 4 }}>
+              <Stack space={2}>
+                <Heading as="h1" level={3}>
+                  Siegfried Minden GmbH
+                </Heading>
+                <Text>Siegfried PharmaChemikalien Minden GmbH</Text>
+                <Text>Karlstrasse 15, 32423 Minden, Germany</Text>
+              </Stack>
+            </Box>
+            <TabList>
+              <TabTrigger value="requests">Requests</TabTrigger>
+              <TabTrigger value="contacts">Contacts</TabTrigger>
+              <TabTrigger value="capa">CAPA</TabTrigger>
+              <TabTrigger value="reports">Reports</TabTrigger>
+              <TabTrigger value="auditee">Auditee</TabTrigger>
+            </TabList>
+          </Container>
         </Box>
-        <TabList>
-          <TabTrigger value="requests">Requests</TabTrigger>
-          <TabTrigger value="audits">Audits</TabTrigger>
-          <TabTrigger value="reports">Reports</TabTrigger>
-        </TabList>
-      </Box>
-      <Box sx={{ paddingTop: 4, px: 3 }}>
-        <TabContent value="requests">
-          Some list of request align with everything else.
-        </TabContent>
-        <TabContent value="audits">Some list of audits</TabContent>
-        <TabContent value="reports">REPORTS!!!</TabContent>
-      </Box>
-    </Tabs>
+        <Box sx={{ py: 4 }}>
+          <Container maxWidth="page">
+            <Box>
+              <TabContent value="requests">
+                {[
+                  'Crystallized ascorbic acid, Ascorbic acid Type EC coated, Dexpanthenol, Calcium Panthotenate',
+                  'Guaifenesinum',
+                  'Crystallized ascorbic acid, Ascorbic acid Type EC coated',
+                ].map(item => (
+                  <Box key={item} sx={{ ...cardStyles }}>
+                    {item}
+                  </Box>
+                ))}
+              </TabContent>
+              <TabContent value="contacts">
+                {['Acme Co.', 'Binford Ltd.', 'Big Kahuna Burger Ltd.'].map(
+                  item => (
+                    <Box key={item} sx={{ ...cardStyles }}>
+                      {item}
+                    </Box>
+                  )
+                )}
+              </TabContent>
+              <TabContent value="capa">CAPA</TabContent>
+              <TabContent value="reports">Reports</TabContent>
+              <TabContent value="auditee">Auditee</TabContent>
+            </Box>
+          </Container>
+        </Box>
+      </Tabs>
+    </Box>
   )
 }
-WithOverview.story = {
-  name: 'with Overview',
+Example.story = {
+  name: 'example from app',
+  parameters: {
+    layout: 'fullscreen',
+  },
 }
