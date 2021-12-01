0e84cd7e80f65dd72bbb162f5bc3b43172d88f9f
Gregor Albrecht <hello@gregoralbrecht.com>
2021-12-01 08:58:31 +0100

    Fix tab styles (#246)
    * Fix styles

--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -9,40 +9,67 @@ const Tabs = styled(TabsPrimitives.Root)(() => ({
 const TabList = styled(TabsPrimitives.List)(props => ({
   'flexShrink': 0,
   'display': 'flex',
-  'borderBottom': `solid 1px ${props.theme.colors.grey[600]} !important`,
-  '&:focus': {
-    borderBottom: `solid 1px ${props.theme.colors.grey[600]} !important`,
-  },
+  'borderBottomStyle': 'solid',
+  'borderBottomWidth': '1px',
+  'borderBottomColor': props.theme.colors.grey[300],
   'overflow-x': 'auto hidden',
+  'marginBottom': '-1px',
 }))
 
 const TabTrigger = styled(TabsPrimitives.Trigger)(props => ({
+  'position': 'relative',
   'color': props.theme.colors.grey[800],
   'fontWeight': props.theme.fontWeights.semibold,
+  'fontSize': props.theme.fontSizes[1],
   'cursor': 'pointer',
-  'padding': `${props.theme.space[1]}px ${props.theme.space[3]}px`,
+  'paddingLeft': props.theme.space[1],
+  'paddingRight': props.theme.space[1],
+  'paddingTop': props.theme.space[2],
+  'paddingBottom': props.theme.space[2],
   'marginLeft': props.theme.space[3],
   'marginRight': props.theme.space[3],
-  'marginBottom': '-2px',
+  'marginBottom': '-1px',
   '&:first-child': { marginLeft: 0 },
   '&:last-child': { marginRight: 0 },
+  'borderBottomStyle': 'solid',
+  'borderBottomWidth': '2px',
+  'borderBottomColor': 'transparent',
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    background: 'transparent',
+    borderRadius: props.theme.radii[2],
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    zIndex: '1',
+    pointerEvents: 'none',
+  },
+  '&:focus-visible': {
+    'outline': 'none',
+
+    '&::after': {
+      outline: 'none',
+      boxShadow: props.theme.shadows.focusRing,
+    },
+  },
   '&[data-state="active"]': {
     color: props.theme.colors.primary[600],
-    borderBottom: `solid 2px ${props.theme.colors.primary[600]}`,
+    borderBottomStyle: 'solid !important',
+    borderBottomWidth: '2px !important',
+    borderBottomColor: props.theme.colors.primary[600],
   },
   '&[data-state="inactive"]:hover': {
     color: props.theme.colors.grey[900],
-  },
-  '&:focus': {
-    color: props.theme.colors.primary[700],
-    borderBottom: `solid 2px ${props.theme.colors.primary[700]} !important`,
-    boxShadow: props.theme.shadows.focusRing,
+    borderBottomColor: props.theme.colors.grey[300],
   },
 }))
 
 const TabContent = styled(TabsPrimitives.Content)(props => ({
-  'outline': 'none',
-  '&:focus': {
+  'borderRadius': props.theme.radii[2],
+  '&:focus-visible': {
+    outline: 'none',
     boxShadow: props.theme.shadows.focusRing,
   },
 }))
