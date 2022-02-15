9fdb4657973a85663b0507811987369a01fcce26
Gregor Albrecht <hello@gregoralbrecht.com>
2022-02-15 11:54:40 +0100

    Repro for CSS stacking context bug with `<Tiles>`
    The `Tiles` component currently has a bug: It "leaks" outside of itself
    because we're using negative margins. More specifically, if you place an
    interactive component (e.g., a button) right adjacent to it, `Tiles` will
    overlap the button and make it unclickable.
    
    We need to make sure interactive elements adjacent to `Tiles` remain
    clickable.
    
    Why does this happen?
    We have a `position: relative;` on all our `Box` components, which means
    they will implicitly create a new stacking context[0]. And we use these
    `Box`es inside `Tiles`.
    
    [0] https://www.joshwcomeau.com/css/stacking-contexts/

--- src/components/Tiles/index.stories.jsx
+++ src/components/Tiles/index.stories.jsx
@@ -1,16 +1,22 @@
+/* eslint-disable no-alert */
 import React from 'react'
-import { text, array } from '@storybook/addon-knobs'
+import { text, select } from '@storybook/addon-knobs'
 
 import Placeholder from '../private/Placeholder'
+import Box from '../Box'
+import Button from '../Button'
 
 import Tiles from './index'
+
+// All possible values for `space` according to our theme
+const ALL_SPACES = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 export default { title: 'Tiles', component: Tiles }
 
 export const Default = () => {
   const as = text('as', 'div')
-  const columns = array('columns', [1, 2, 3])
-  const space = array('space', [1, 3, 5])
+  const columns = select('Columns', [1, 2, 3, 4], 3)
+  const space = select('Space', ALL_SPACES, 5)
 
   return (
     <Tiles columns={columns} as={as} space={space}>
@@ -28,4 +34,32 @@ export const Default = () => {
 }
 Default.story = {
   name: 'default',
+}
+
+export const Adjacent = () => {
+  const as = text('as', 'div')
+  const columns = select('Columns', [1, 2, 3, 4], 3)
+  const space = select('Space', ALL_SPACES, 5)
+
+  return (
+    <Box>
+      <Box>
+        <Button onClick={() => alert('Yes I am!')}>Am I clickable?</Button>
+      </Box>
+      <Tiles columns={columns} as={as} space={space}>
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+        <Placeholder height={50} />
+      </Tiles>
+    </Box>
+  )
+}
+Adjacent.story = {
+  name: 'with adjacent interactive elements',
 }
