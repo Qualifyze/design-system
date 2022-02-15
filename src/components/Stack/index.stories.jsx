a6dc0b3fa61b049417d4eb01bd7dd6623f29376b
Gregor Albrecht <hello@gregoralbrecht.com>
2022-02-15 11:03:37 +0100

    Repro for CSS stacking context bug with `<Stack>`
    The `Stack` component currently has a bug: It "leaks" outside of itself
    because we're using negative margins. More specifically, if you place an
    interactive component (e.g., a button) right above it, the `Stack` will
    overlap the button and make it unclickable.
    
    We need to make sure interactive elements right above `Stack` remain
    clickable.
    
    Why does this happen?
    We have a `position: relative;` on all our `Box` components, which means
    they will implicitly create a new stacking context[0].
    
    [0] https://www.joshwcomeau.com/css/stacking-contexts/

--- src/components/Stack/index.stories.jsx
+++ src/components/Stack/index.stories.jsx
@@ -1,21 +1,51 @@
+/* eslint-disable no-alert */
 import React from 'react'
+import { select } from '@storybook/addon-knobs'
 
-import Heading from '../Heading'
-import Text from '../Text'
+import Box from '../Box'
+import Button from '../Button'
+import Inline from '../Inline'
+import Placeholder from '../private/Placeholder'
 
 import Stack from './index'
+
+// All possible values for `space` according to our theme
+const ALL_SPACES = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 export default { title: 'Stack', component: Stack }
 
 export const Default = () => {
+  const space = select('Space', ALL_SPACES, 3)
+
   return (
-    <Stack space={4} align="left">
-      <Heading>Heading goes here</Heading>
-      <Text>Text goes here</Text>
-      <Text>And even more goes here</Text>
+    <Stack space={space}>
+      <Placeholder height={50} width="100%" />
+      <Placeholder height={50} width="100%" />
+      <Placeholder height={50} width="100%" />
+      <Placeholder height={50} width="100%" />
     </Stack>
   )
 }
 Default.story = {
   name: 'default',
+}
+
+export const Adjacent = () => {
+  const space = select('Space', ALL_SPACES, 3)
+
+  return (
+    <Box>
+      <Inline>
+        <Button onClick={() => alert('I am!')}>Am I clickable?</Button>
+      </Inline>
+      <Stack space={space}>
+        <Placeholder height={50} width="100%" />
+        <Placeholder height={50} width="100%" />
+        <Placeholder height={50} width="100%" />
+      </Stack>
+    </Box>
+  )
+}
+Adjacent.story = {
+  name: 'with adjacent interactive elements',
 }
