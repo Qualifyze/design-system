0d5cf40c49a9478fe70c5f92b241ec58141a9401
Gregor Albrecht <hello@gregoralbrecht.com>
2022-04-14 12:59:48 +0200

    Drop IE11 for `Tiles` in favor of flexbox's `gap`
    We decided it's time to drop IE11 support for our `Tiles` component in
    favor of using `flexbox`'s `gap` property. Previously, we tried to avoid
    `gap` (because it's not supported by IE11) but this has lead to some
    gnarly issues with negative margins.

--- src/components/Tiles/__tests__/useAlignment.js
+++ src/components/Tiles/__tests__/useAlignment.js
@@ -2,17 +2,16 @@ import useAlignment from '../useAlignment'
 
 describe('useAlignment', () => {
   test('when you pass a single value you get the expected styles', () => {
-    const alignRight = useAlignment('right')
-    const alignLeft = useAlignment('left')
-    const alignCenter = useAlignment('center')
-
-    expect(alignRight).toEqual('flex-end')
-    expect(alignLeft).toEqual('flex-start')
-    expect(alignCenter).toEqual('center')
+    expect(useAlignment('right')).toEqual('flex-end')
+    expect(useAlignment('left')).toEqual('flex-start')
+    expect(useAlignment('center')).toEqual('center')
   })
 
   test('when you pass a responsive array you should get out an array', () => {
-    const responsiveAlignment = useAlignment(['right', 'left', 'center'])
-    expect(responsiveAlignment).toEqual(['flex-end', 'flex-start', 'center'])
+    expect(useAlignment(['right', 'left', 'center'])).toEqual([
+      'flex-end',
+      'flex-start',
+      'center',
+    ])
   })
 })
