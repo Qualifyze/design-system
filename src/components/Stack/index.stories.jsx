5b5c51ff71e82f4b3b7a05169e4c307e5f3e8b58
Gregor Albrecht <hello@gregoralbrecht.com>
2022-04-14 17:26:20 +0200

    Drop IE11 for `Stack` in favor of flexbox's `gap`
    We decided it's time to drop IE11 support for our `Stack` component in
    favor of using `flexbox`'s `gap` property. Previously, we tried to avoid
    `gap` (because it's not supported by IE11) but this has lead to some
    gnarly issues with negative margins.

--- src/components/Stack/index.stories.jsx
+++ src/components/Stack/index.stories.jsx
@@ -1,13 +1,8 @@
 /* eslint-disable no-alert */
 import React from 'react'
-import { select, boolean } from '@storybook/addon-knobs'
+import { select } from '@storybook/addon-knobs'
 
-import Box from '../Box'
-import Button from '../Button'
-import Inline from '../Inline'
 import Placeholder from '../private/Placeholder'
-import Flex from '../Flex'
-import Text from '../Text'
 
 import Stack from './index'
 
@@ -30,60 +25,4 @@ export const Default = () => {
 }
 Default.story = {
   name: 'default',
-}
-
-export const Adjacent = () => {
-  const space = select('Space', ALL_SPACES, 3)
-
-  return (
-    <Box>
-      <Inline>
-        <Button onClick={() => alert('I am!')}>Am I clickable?</Button>
-      </Inline>
-      <Stack space={space}>
-        <Placeholder height={50} width="100%" />
-        <Placeholder height={50} width="100%" />
-        <Placeholder height={50} width="100%" />
-      </Stack>
-    </Box>
-  )
-}
-Adjacent.story = {
-  name: 'with adjacent interactive elements',
-}
-
-export const LeakingExample = () => {
-  const space = select('Space', ALL_SPACES, 6)
-  const withIndex = boolean('Using z-index', true)
-
-  return (
-    <Box sx={{ border: 'solid 3px black' }}>
-      <Text>Check the README for an explanation</Text>
-      <Flex
-        sx={{
-          zIndex: withIndex ? '2' : null,
-        }}
-      >
-        <Inline>
-          <Button onClick={() => alert('Yes, I am!')}>
-            But am I clickable? Try me with and without z-index and a space
-            value above 4
-          </Button>
-        </Inline>
-      </Flex>
-      <Box sx={{ zIndex: withIndex ? '1' : null, border: 'solid 3px red' }}>
-        <Stack space={space}>
-          <Inline>
-            <Button onClick={() => alert('I am!')}>Am I clickable?</Button>
-          </Inline>
-          <Placeholder height={50} width="100%" />
-          <Placeholder height={50} width="100%" />
-          <Placeholder height={50} width="100%" />
-        </Stack>
-      </Box>
-    </Box>
-  )
-}
-LeakingExample.story = {
-  name: 'leaking example',
 }
