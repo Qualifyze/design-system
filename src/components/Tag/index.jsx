e9d908739a1cbcd30ca837029c00f46c4593150f
BartekQuali <bartosz.kakol@qualifyze.com>
2022-04-28 17:07:14 +0200

    VE-512 Add new tag design

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,53 +1,138 @@
 import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 
-import { styled } from '../../util/style'
+import { styled, theme } from '../../util/style'
 import Text from '../Text'
 import Box from '../Box'
 
-const Base = styled('span')(props => ({
+const TagSize = {
+  large: {
+    textSize: 'standard',
+    tagHeight: '30px',
+    hitHeight: '44px',
+  },
+  small: {
+    textSize: 'small',
+    tagHeight: '24px',
+    hitHeight: '32px',
+  },
+}
+
+const TagColors = {
+  info: {
+    content: theme.colors.primary[700],
+    background: theme.colors.primary[100],
+    hoverContent: theme.colors.primary[800],
+    hoverBackground: theme.colors.primary[200],
+  },
+  positive: {
+    content: theme.colors.green[700],
+    background: theme.colors.green[100],
+    hoverContent: theme.colors.green[800],
+    hoverBackground: theme.colors.green[200],
+  },
+  neutral: {
+    content: theme.colors.grey[700],
+    background: theme.colors.grey[200],
+    hoverContent: theme.colors.grey[800],
+    hoverBackground: theme.colors.grey[300],
+  },
+  caution: {
+    content: theme.colors.yellow[800],
+    background: theme.colors.yellow[200],
+    hoverContent: theme.colors.yellow[900],
+    hoverBackground: theme.colors.yellow[300],
+  },
+  critical: {
+    content: theme.colors.red[700],
+    background: theme.colors.red[100],
+    hoverContent: theme.colors.red[800],
+    hoverBackground: theme.colors.red[200],
+  },
+}
+
+const Base = styled('div')((props) => ({
   'position': 'relative',
-  'paddingLeft': props.theme.space[2],
+  'boxSizing': 'content-box',
   'paddingRight': props.theme.space[2],
+  'paddingLeft': '0px',
+  'paddingTop': '0px',
+  'paddingBottom': '0px',
   'whiteSpace': 'nowrap',
+  'height': props.tagSize.tagHeight,
+  'display': 'flex',
+  'justifyContent': 'center',
+  'backgroundColor': props.color.background,
+  'borderRadius': props.theme.radii[4],
   'cursor': props.onClick ? 'pointer' : 'text',
+  'alignItems': 'center',
+  'border': 'none',
+  '&::after': {
+    content: '""',
+    boxSizing: 'content-box',
+    position: 'absolute',
+    transform: 'translateY(-50%)',
+    paddingLeft: '4px',
+    paddingRight: '4px',
+    height: props.tagSize.hitHeight,
+    width: '100%',
+    top: '50%',
+    left: '-4px',
+    right: '0',
+  },
   '& > span': {
-    'color': props.theme.colors.grey[700],
-    '&::after': {
-      content: '""',
-      position: 'absolute',
-      transform: 'translateY(-50%)',
-      background: props.theme.colors.grey[200],
-      borderRadius: props.theme.radii[3],
-      minHeight: '30px',
-      top: '50%',
-      left: '0',
-      right: '0',
-      zIndex: '-1',
+    'color': props.color.content,
+    'marginLeft': props.theme.space[2],
+    '& > svg': {
+      'width': '12px',
+      'height': '12px',
     },
   },
   '&:hover': {
+    'backgroundColor': props.color.hoverBackground,
     '& > span': {
-      'color': props.theme.colors.grey[900],
-      '&::after': {
-        background: props.theme.colors.grey[400],
-      },
+      'color': props.color.hoverContent,
     },
+  },
+  '&:focus-visible': {
+    'border': '4px solid #478FE4',
+    'boxShadow': 'none',
+    'outline': 'none',
   },
 }))
 
-const Tag = forwardRef(({ children, as, icon, onClick }, ref) => (
-  <Base as={as} ref={ref} onClick={onClick}>
-    <Text as="span" size="small" weight="medium">
-      {children}
-    </Text>
-    {icon ? (
-      <Box as="span" sx={{ pl: 2 }}>
-        {icon}
-      </Box>
-    ) : null}
-  </Base>
-))
+const Tag = forwardRef(
+  ({ children, as, icon, trailingIcon, onClick, color, tagSize }, ref) => (
+    <Base
+      as={onClick ? 'button' : as}
+      ref={ref}
+      color={color}
+      tagSize={tagSize}
+      onClick={onClick}
+    >
+      {icon ? <Box as="span">{icon}</Box> : null}
+      <Text as="span" weight="medium" size={tagSize.textSize}>
+        {children}
+      </Text>
+      {trailingIcon ? (
+        <Box
+          as="span"
+          sx={{
+            bg: 'white',
+            display: 'flex',
+            borderRadius: '50%',
+            height: '20px',
+            width: '20px',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {trailingIcon}
+        </Box>
+      ) : null}
+    </Base>
+  )
+)
 
 Tag.displayName = 'Tag'
 
@@ -55,6 +140,9 @@ Tag.defaultProps = {
   as: 'span',
   onClick: null,
   icon: null,
+  trailingIcon: null,
+  color: TagColors.neutral,
+  tagSize: TagSize.large,
 }
 
 Tag.propTypes = {
@@ -63,6 +151,9 @@ Tag.propTypes = {
   onClick: PropTypes.func,
   children: PropTypes.string.isRequired,
   icon: PropTypes.node,
+  trailingIcon: PropTypes.node,
+  color: PropTypes.object,
+  tagSize: PropTypes.object,
 }
 
 export default Tag
