856ce2ae233f0a9e7a4878acea3b59c5b70ded03
Philipp Jardas <philipp.jardas@qualifyze.com>
2022-09-07 11:19:04 +0200

    Use colors from new branding

--- src/__tests__/matching-emotion-versions.test.jsx
+++ src/__tests__/matching-emotion-versions.test.jsx
@@ -3,12 +3,9 @@ import React from 'react'
 import { render, screen } from '../util/test'
 import { styled, shouldForwardProp } from '../util/style'
 
-const RandomStyledComponent = styled('div', { shouldForwardProp })(props => {
-  // console.log('props', props)
-  return {
-    color: props.theme.colors.primary[700],
-  }
-})
+const RandomStyledComponent = styled('div', { shouldForwardProp })(props => ({
+  color: props.theme.colors.primary[700],
+}))
 
 /**
  * We need to ensure we install matching versions of Emotion (`@emotion/styled`
@@ -25,5 +22,5 @@ test('Ensure matching versions of Emotion', () => {
   render(<RandomStyledComponent>hola</RandomStyledComponent>)
   const Component = screen.getByText(/hola/)
   expect(Component).toMatchSnapshot()
-  expect(Component).toHaveStyle({ color: 'hsl(223, 60%, 29%)' }) // Our primary.700 color
+  expect(Component).toHaveStyle({ color: 'hsl(240, 45%, 34%)' }) // Our primary.700 color
 })
